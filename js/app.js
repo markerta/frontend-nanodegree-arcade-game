8ec27f21289e8c34b43e82d8dc1fc9c4249d282c
markerta <markert.manda@gmail.com>
2017-07-18 13:27:17 -0400

    Update app.js to add Enemy and Player objects, and basic movement functionality

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.row = Math.floor(Math.random() * 2) + 1;
+    this.col = -1;
+    this.x = this.col * 101;
+    this.y = this.row * 83;
+    this.speed = (Math.floor(Math.random() * 5) + 1) * 10;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + (this.speed * dt);
+
+    // Handle collisions
+
+    // Reset or unrender enemy who has left edge of screen right
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +34,64 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+	this.sprite = 'images/char-boy.png';
+	this.reset();
+	this.speed = 1;
+}
+Player.prototype.update = function() {
+	// Did he reach the water? If yes, reset
+	if (this.y < 0) {
+		this.reset();
+	}
+};
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(input) {
+	var prevX = this.x;
+	var prevY = this.y;
 
+	switch(input) {
+		case 'left' : 
+			if (prevX > 0) {
+				this.x = prevX - 101;
+			}
+			break;
+		case 'up' :
+			if (prevY >= 0) {
+				this.y = prevY - 83;
+			}
+			break;
+		case 'right' :
+			if (prevX < 404) {
+				this.x = prevX + 101;
+			}
+			break;
+		case 'down' :
+			if (prevY < 404) {
+				this.y = prevY + 83;
+			}
+			break;
+	}
+};
+
+Player.prototype.reset = function() {
+	this.row = 5;
+	this.col = 2;
+	this.x = this.col * 101;
+	this.y = this.row * 83;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for(var i = 0; i < 3; i++){
+    allEnemies.push(new Enemy());
+}
+
+var player = new Player();
 
 
 
